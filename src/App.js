de4a5dbeef65e13d4c1ad51d0a4eca050e8836b9
atulj10 <atulanandj10@gmail.com>
2024-02-26 19:02:20 +0530

    Package Downloads

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
       E Services
+      <FontAwesomeIcon icon={faHome}/>
+      <button className='btn btn-outline-dark'>Click</button>
     </div>
   );
 }
